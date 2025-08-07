7deb3caae55a3364a70d4afc43d50772cd06fe96
Sarumathy07 <sarumathy1611@gmail.com>
2025-08-07 20:49:06 +0530

    Add files via upload

---
+++ Booklist/app.js
@@ -0,0 +1,59 @@
+const express=require('express');
+const app=express();
+const PORT=3000;
+
+app.use(express.json());
+
+
+let books =[];
+let nextId= 1;
+
+app.get('/books',(req, res)=>{
+     res.json(books);
+});
+
+app.post('/books',(req, res)=>{
+  
+      const {title, author}=req.body;
+    
+      if(!title || !author){
+        return res.status(400).json({error:'Title and author required'});
+      }
+    
+      const newBook={id: nextId++, title, author};
+      books.push(newBook);
+      res.status(201).json(newBook);
+});
+
+app.put('/books/:id', (req, res)=>{
+      const bookId= parseInt(req.params.id);
+      const {title, author} = req.body;
+      const book = books.find(b=>b.id === bookId);
+      
+            if(!book){
+              return res.status(404).json({error:'Book not found'});      
+           }
+             if(title) book.title=title;
+             if(author) book.author=author;
+             res.json(book);
+             
+  });
+  
+  
+  app.delete('/books/:id', (req, res)=>{
+      const bookId=parseInt(req.params.id);
+      const index= books.findIndex(b=>b.id===bookId);
+      
+            if(index===-1){
+              return res.status(404).json({error:'book not found'});
+  }
+  
+  
+  books.splice(index, 1);
+  res.json({message: 'book deleted'});
+  });
+  
+  
+  app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
+            
+            
